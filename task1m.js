7ece1d05a43137fef88962085e6fd571a6f947c6
Anupriya759 <simi152006@gmail.com>
2025-07-06 21:17:22 +0530

    Delete task1m.js

--- task1m.js
+++
@@ -1,9 +0,0 @@
-// Change nav style on scroll
-window.addEventListener('scroll', () => {
-  const navbar = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
